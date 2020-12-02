8e0e69d4ffb0eeea136550fd85cb47c250664708
Kee <47179236+rushkii@users.noreply.github.com>
2020-12-02 20:40:35 +0700

    Update __init__.py

--- maid_manga/__init__.py
+++ maid_manga/__init__.py
@@ -4,6 +4,6 @@ __copyright__       = 'Copyright 2020 by https://maid.my.id/'
 __version__         = '1.0'
 __author__          = 'Kee'
 __author_email__    = 'riskimuhammmad1@gmail.com'
-__url__             = 'https://github.com/rushkii/MaidMangaID'
+__url__             = 'https://github.com/rushkii/maid_manga_id'
 
 __all__ = ['MaidMangaID']
